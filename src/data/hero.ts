fbe15380476679a752695096ab49b82580c5cdd7
S Deepak Kumar <dipkfilms@gmail.com>
2025-04-06 17:19:13 +0530

    fixed

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -3,7 +3,7 @@ import rightImage from '/public/images/star.svg';
 
 export const heroDetails = {
     heading: 'Smart, Secure, Simple Financial Management',
-    subheading: 'Create the perfect cover photo for Facebook, Twitter, YouTube, LinkedIn, and Etsy. Choose from thousands of professionally designed templates, access millions of royalty-free images, and design with ease using our intuitive tool.',
+    subheading: 'Create the perfect cover photo for Facebook, Twitter, YouTube, LinkedIn, and Instagram. Choose from thousands of professionally designed templates, access millions of royalty-free images, and design with ease using our intuitive tool.',
     leftImageSrc: leftImage,
     rightImageSrc: rightImage,
 }
