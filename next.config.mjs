99551ca1df2a9b59ff9976335a37eef65853ddc5
S Deepak Kumar <dipkfilms@gmail.com>
2025-04-06 16:36:24 +0530

    config changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async redirects() {
-      return [
-        {
-          source: "/((?!api|join-waitlist).*)", // Allow /api/* and /join-waitlist
-          destination: "/join-waitlist",
-          permanent: false,
-        },
-      ];
+        return [
+            {
+                source: "/((?!api|join-waitlist|_next|public).*)",
+                destination: "/join-waitlist",
+                permanent: false,
+            },
+        ];
     },
-  };
-  
-  export default nextConfig;
-  
+};
+
+export default nextConfig;
