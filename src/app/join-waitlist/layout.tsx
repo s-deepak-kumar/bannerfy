fbc5de949a1b1f845371190c4ae441cd323925bd
S Deepak Kumar <dipkfilms@gmail.com>
2025-04-08 12:33:48 +0530

    Added

--- src/app/join-waitlist/layout.tsx
+++ src/app/join-waitlist/layout.tsx
@@ -9,7 +9,7 @@ const FigtreeFont = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: siteDetails.metadata.title,
-  description: siteDetails.metadata.description
+  description: siteDetails.metadata.description,
 };
 
 export default function RootLayout({
@@ -19,23 +19,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
-      <meta property="og:image" content="https://www.brandigo.io/images/twitter-image.png" />
+      <meta
+        property="og:image"
+        content="https://www.brandigo.io/images/twitter-image.png"
+      />
       <meta property="og:image:type" content="image/png" />
       <meta property="og:image:width" content="1280" />
       <meta property="og:image:height" content="832" />
-      <meta
-        property="og:site_name"
-        content={siteDetails.metadata.title}
-      />
-      <meta
-        property="og:url"
-        content="https://brandigo.io/"
-      />
+      <meta property="og:site_name" content={siteDetails.metadata.title} />
+      <meta property="og:url" content="https://brandigo.io/" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://www.brandigo.io/images/twitter-image.png" />
+      <meta
+        name="twitter:url"
+        content="https://www.brandigo.io/join-waitlist"
+      />
+      <meta name="twitter:title" content={siteDetails.metadata.title} />
+      <meta
+        name="twitter:description"
+        content={siteDetails.metadata.description}
+      />
+      <meta
+        name="twitter:image"
+        content="https://www.brandigo.io/images/twitter-image.png"
+      />
       <meta name="twitter:image:type" content="image/png" />
       <meta name="twitter:image:width" content="1280" />
       <meta name="twitter:image:height" content="832" />
+
       <body className={`${FigtreeFont.className} overflow-hidden`}>
         {children}
         <Toaster richColors position="top-center" />
